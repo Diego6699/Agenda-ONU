27f5fabcd4bd99030f970ef3c01de4d3c1061efd
Diego Leonardo da Silva FRança <diegoleonardo9966@gmail.com>
2022-06-11 18:57:02 -0300

    formatando imagens e videos

---
+++ partials/navbar.php
@@ -0,0 +1,13 @@
+<div class="navbar-fixed">
+  <nav>
+      <div class="nav-wrapper yellow">
+        <a href="#" class="brand-logo"><span class="black-text"><i class="fas fa-power-off blue-text"></i> Energia limpa e acessível</span></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a class="black-text" href="/Agenda-ONU/index.php">Home</a></li>
+          <li><a class="black-text" href="https://brasil.un.org/pt-br/sdgs" target="_blank">Agenda da Onu 2030</a></li>
+          <li><a class="black-text" href="/Agenda-ONU/pages/sobrenos.php">Sobre o site</a></li>
+          <li><a class="black-text" href="/Agenda-ONU/pages/avaliacao.php">Dê sua opinião</a></li>
+        </ul>
+      </div>
+    </nav>
+</div>
